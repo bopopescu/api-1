10886e4c21d05a7dc0acb51a54fe97a6c4cdc5d5
Al Janabi Sarah <sarah.aljanabi@gmail.com>
2018-08-13 08:59:45 +0000

    blabl

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,7 @@
-from flask import Flask
-from werkzeug.serving import run_simple
 from __init__ import app as application
+from flask import Flask
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 if __name__ == "__main__":
     application.run()
-run_simple('0.0.0.0', 8000, app, use_reloader=True)
