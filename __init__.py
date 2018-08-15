fca04a585f2ec89e79cb6a5dce934e61ab3c677c
Al Janabi Sarah <sarah.aljanabi@gmail.com>
2018-08-15 15:25:20 +0000

    step 4

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ from flask import Response
 from mysql.connector import Error
 from phpserialize import *
 from collections import OrderedDict
+from slugify import slugify
 
 app = Flask(__name__)
 
@@ -24,29 +25,29 @@ def getConnector():
         print(e)
         return False
 
-
 @app.errorhandler(400)
 def error400():
     return Response(json.dumps(
         {"code": 400,
          "message": "Bad Request"}
     ), 401, {'Content-Type': 'Application/json'})
-    return json.dumps({"code" : 400, "message" : "Bad Request"}), 400
+
 @app.errorhandler(401)
 def error401():
-        return Response(json.dumps(
-            {"code": 401,
-             "message": "Unauthorized"}
-        ), 401, {'Content-Type': 'Application/json'})
+    return Response(json.dumps(
+        {"code": 401,
+         "message": "Unauthorized"}
+    ), 401, {'Content-Type': 'Application/json'})
 
 @app.errorhandler(403)
 def error403():
     return Response(json.dumps(
-            {"code" : 403,
-             "message" : "Forbidden"}
+        {"code" : 403,
+         "message" : "Forbidden"}
     ), 403, {'Content-Type': 'Application/json'})
+
 @app.errorhandler(404)
-def error404():
+def error404(arg):
     return Response(json.dumps(
                 {"code" : 404,
                 "message" : "Not Found"}
@@ -59,8 +60,7 @@ def jsonResponse(data = {}, message = '', status = 200):
                 "message": message,
                 "datas": data
               }), status, {
-              'Content-Type': 'Application/json'
-        })
+              'Content-Type': 'Application/json'})
 
 @app.route('/')
 def index():
@@ -112,7 +112,7 @@ def getRecipeBySlug(slug):
     rows = cursor.fetchone()
 
     if rows is None:
-        return error404()
+        return Response(json.dumps({'code' : 404, 'message' : 'not found'}), 404, {'Content-Type' : 'Application/json'})
 
     data = {};
     data['id'] = rows['recipe_id']
@@ -151,7 +151,7 @@ def getStepsForRecipe(slug):
     row = cursor.fetchone()
 
     if row is None:
-        return error404()
+        return Response(json.dumps({'code' : 404, 'message' : 'not found'}), 404, {'Content-Type': 'Application/json'})
 
     steps_load = loads(row['step'].encode(), array_hook=OrderedDict)
     res = []
@@ -164,9 +164,88 @@ def getStepsForRecipe(slug):
         'message' : 'ok',
         'datas' : res
     })
-
     return Response(responseFinal, 200, {'Content-Type': 'Application/json'})
 
+# Step 4
+@app.route('/api/recipes.json', methods=['POST'])
+def createRecipe():
+    conn = getConnector()
+    if not conn:
+        return Response(json.dumps(
+            {
+                "code":500,
+                "message" : "no connector"
+            }), 500, {
+            'Content-Type': 'Application/json'
+        })
+
+    cursor = conn.cursor(dictionary=True)
+
+    # Si le mot de passe ne correspond a aucun user en base
+    userpass = request.headers['Authorization']
+    if (userpass is None):
+        return Response(json.dumps({'code' : 401, 'message': 'bad request'}), 401, {'Content-Type': 'Application/json'})
+
+    check_user_query = "SELECT id, username, last_login FROM users__user WHERE password = %s LIMIT 1;"
+    cursor.execute(check_user_query, (userpass,))
+    row_user = cursor.fetchone()
+    if row_user is None:
+        return Response(json.dumps({'code' : 403, 'message': 'invalid password'}), 403, {'Content-Type': 'Application/json'})
+
+    user_id = row_user['id']
+
+    # Récupération des données du formulaire
+    post_data = request.form
+    steps = post_data.getlist('step[]')
+    name = post_data.get('name')
+
+    slug = post_data.get('slug', default=None)
+    # Si le slug est précisé, je vérifie qu'une recette avec le meme slug n'existe pas
+    if slug is not None:
+        check_slug_query = "SELECT slug FROM recipes__recipe WHERE slug = %s;"
+        cursor.execute(check_slug_query, (slug,))
+        row = cursor.fetchone()
+        if row is not None:
+            return Response(json.dumps({'code' : 400, 'message' : 'bad request'}), 400, {'Content-Type': 'Application/json'})
+    # Si le slug nest pas précisé, je génère un slug a partir du nom.
+    else:
+        slug = slugify(name)
+        check_slug_query = "SELECT slug FROM recipes__recipe WHERE slug = %s;"
+        cursor.execute(check_slug_query, (slug,))
+        row = cursor.fetchone()
+        # Si le slug généré existe déjà, je rajoute des caracteres aleatoires apres le slug généré
+        if row is not None:
+            slug = name + '-' + ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(5))
+
+
+    # Sérialisation des étapes pour les insérer en base
+    steps_s = serialize(steps)
+    # Insertion en base de données de la nouvelle recette
+    create_query = "INSERT INTO recipes__recipe(name, slug, step, user_id) VALUES (%s, %s, %s, %s)"
+    cursor.execute(create_query, (name, slug, steps_s, user_id,))
+    conn.commit()
+
+    id = cursor.lastrowid
+    cursor.close()
+
+    responseData = json.dumps({
+        'code' : 201,
+        'message' : 'Created',
+        'datas' : {
+            'id' : id,
+            'name' : name,
+            'slug' : 'slug',
+            'user' : {
+                'username' : row_user['username'],
+                'last_login' : row_user['last_login'].strftime('%Y-%m-%dT%H:%M:%S %z'),
+                'id' : user_id
+            },
+            'slug' : slug,
+            'step' : steps
+        }
+    })
+
+    return Response(responseData, 201, {'Content-Type' : 'Application/json'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
