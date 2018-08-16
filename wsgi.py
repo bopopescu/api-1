5e3cc15f5d52e576b8b98c0523a721827f300cb1
Sarah Al Janabi <sarah.aljanabi@gmail.com>
2018-08-16 17:44:25 +0000

    uploaded app to another server

--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,3 @@ from werkzeug.serving import run_simple
 if __name__ == "__main__":
     app.run()
 
-#app = Flask(__name__)
-
-#if __name__ == "__main__":
-#    application.run()
-#run_simple('0.0.0.0', 000, application)
