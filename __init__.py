530e86b8e3557e2b6f6e9a8ea8bd2f5467f7d360
Sarah Al Janabi <sarah.aljanabi@gmail.com>
2018-08-18 23:31:36 +0000

    step 6

--- __init__.py
+++ __init__.py
@@ -251,5 +251,175 @@ def createRecipe():
 
     return Response(responseData, 201, {'Content-Type' : 'Application/json'})
 
+# Step 5
+@app.route('/api/recipes/<slug>.json', methods=['PUT'])
+def updateRecipeBySlug(slug):
+    conn = getConnector()
+    if not conn:
+        return Response(json.dumps(
+            {
+                "code":200,
+                "message" : "no connector"
+            }), 200, {
+            'Content-Type': 'Application/json'
+        })
+
+    cursor = conn.cursor(dictionary=True)
+    query = "SELECT recipe.id as recipe_id, recipe.user_id as user_id FROM recipes__recipe recipe  WHERE slug = %s LIMIT 1;"
+    cursor.execute(query, (slug,))
+    rows = cursor.fetchone()
+
+    if rows is None:
+        return Response(json.dumps({'code' : 404, 'message' : 'not found', 'data' : {}}), 404, {'Content-Type' : 'Application/json'})
+
+    userpass = request.headers.get('Authorization', default = None)
+    if (userpass is None):
+        return Response(json.dumps({'code' : 401, 'message': 'Unauthorized', 'data' : {}}), 401, {'Content-Type': 'Application/json'})
+
+    check_user_query = "SELECT id, username, last_login FROM users__user WHERE password = %s LIMIT 1;"
+    cursor.execute(check_user_query, (userpass,))
+    row_user = cursor.fetchone()
+    if row_user is None:
+        return Response(json.dumps({'code' : 403, 'message': 'Invalid Password', 'data' : {}}), 403, {'Content-Type': 'Application/json'})
+
+    user_id = row_user['id']
+    if rows['user_id'] != row_user['id'] :
+        return Response(json.dumps({'code' : 403, 'message': 'Invalid Password !', 'data' : {}}), 403, {'Content-Type': 'Application/json'})
+
+
+    userdata = request.form
+    newname = userdata.get('name', default = None)
+    newslug = userdata.get('slug', default = None)
+    newstep = userdata.getlist('step[]')
+
+    data = OrderedDict();
+    if newname is not None :
+        data['name'] = newname
+    if newname == '' :
+        return Response(json.dumps({'code' : 400, 'message': 'Bad Request', 'datas' : []}), 400, {'Content-Type': 'Application/json'})
+    if newslug == '' :
+        return Response(json.dumps({'code' : 400, 'message': 'Bad Request', 'datas' : []}), 400, {'Content-Type': 'Application/json'})
+
+    if newslug is not None :
+        data['slug'] = newslug
+        #On vérifie que le new slug n'est pas déjà pris
+        checknewslugquery = "SELECT recipe.id FROM recipes__recipe recipe WHERE slug = %s LIMIT 1;"
+        cursor.execute(checknewslugquery, (newslug,))
+        if (cursor.fetchone() is not None) :
+            return Response(json.dumps({'code' : 400, 'message' : 'bad request', 'data' : {}}), 400, {'Content-Type': 'Application/json'})
+
+    if newstep is not None :
+        data['step'] = serialize(newstep)
+
+    strquery = ''
+    params = {}
+
+    for key, value in data.items():
+        if (len(strquery) > 0):
+            strquery = strquery + ', '
+        strquery = strquery + key + ' = %(' +  key + ')s'
+        params[key] = value
+
+    params['original_slug'] = slug
+
+    updatequery = "UPDATE recipes__recipe SET " + strquery + " WHERE slug = %(original_slug)s"
+    print(updatequery)
+    cursor.execute(updatequery, params)
+    conn.commit()
+
+    slug = params.get('slug', None)
+    if ( slug is None ) :
+        slug = params['original_slug']
+
+
+    query = "SELECT recipe.id as recipe_id, recipe.name as recipe_name, recipe.slug as slug, recipe.step FROM recipes__recipe recipe WHERE slug = %s LIMIT 1;"
+    cursor.execute(query, (slug,))
+    rows = cursor.fetchone()
+
+    if rows is None:
+        return Response(json.dumps({'code' : 500, 'message' : 'Erreur imprévue'}), 500, {'Content-Type' : 'Application/json'})
+
+    data = {};
+    data['id'] = rows['recipe_id']
+    data['name'] = rows['recipe_name']
+    data['slug'] = rows['slug']
+
+    steps_load = loads(rows['step'].encode(), array_hook=OrderedDict)
+    res = []
+    for index in steps_load:
+        res.append(steps_load[index].decode('utf-8'))
+
+    data['step'] = res
+
+    return Response(json.dumps(
+             {
+                "code": 200,
+                "message": "ok",
+                "datas": data
+              }), 200, {
+              'Content-Type': 'Application/json'
+        })
+
+# Step 6
+@app.route('/api/recipes/<slug>.json', methods=['DELETE'])
+def deleteRecipeBySlug(slug):
+    conn = getConnector()
+    if not conn:
+        return Response(json.dumps(
+            {
+                "code":200,
+                "message" : "no connector"
+            }), 200, {
+            'Content-Type': 'Application/json'
+        })
+
+    cursor = conn.cursor(dictionary=True)
+
+
+    # Si le mot de passe ne correspond a aucun user en base
+    userpass = request.headers.get('Authorization', default = None)
+    if (userpass is None):
+        return Response(json.dumps({'code' : 401, 'message': 'Unauthorized'}), 401, {'Content-Type': 'Application/json'})
+
+    check_user_query = "SELECT id, username, last_login FROM users__user WHERE password = %s LIMIT 1;"
+    cursor.execute(check_user_query, (userpass,))
+    row_user = cursor.fetchone()
+    if row_user is None:
+        return Response(json.dumps({'code' : 403, 'message': 'Invalid Password'}), 403, {'Content-Type': 'Application/json'})
+
+
+    user_id = row_user['id']
+
+    #query = "DELETE recipe.id as recipe_id, recipe.slug as slug, recipe.name as recipe_name, user.username, user.id as user_id FROM recipes__recipe recipe INNER JOIN users__user user ON user.id = recipe.user_id WHERE slug = %s LIMIT 1;"
+    #query = "DELETE recipe.id as recipe_id, recipe.name as recipe_name, recipe.slug as slug, user.username, user.last_login as last_login, user.id as user_id FROM recipes__recipe recipe INNER JOIN users__user user ON user.id = recipe.user_id WHERE slug = %s LIMIT 1;"
+    query = "SELECT recipe.id, recipe.user_id as user_id FROM recipes__recipe recipe  WHERE slug = %s LIMIT 1;"
+    cursor.execute(query, (slug,))
+    row_recipe = cursor.fetchone();
+
+    if row_recipe is None :
+	#erreur pas de recette avec slug
+        return Response(json.dumps({'code' : 400, 'message': 'Bad Request'}), 400, {'Content-Type': 'Application/json'})
+
+    if row_recipe['user_id']  != user_id:
+	#erreur forbidden
+        return Response(json.dumps({'code' : 403, 'message': 'Invalid Password'}), 403, {'Content-Type': 'Application/json'})
+
+    dlquery = "DELETE recipes__recipe WHERE id = %d"
+    cursor.execute(dlquery, (row_recipe[id],))
+    rows = cursor.fetchone()
+    conn.commit()
+
+    return Response(json.dumps(
+             {
+                "code": 200,
+                "message": "success",
+                "datas":{
+                    'id' : row_recipe['id']
+		}
+              }), 200, {
+              'Content-Type': 'Application/json'
+})
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
